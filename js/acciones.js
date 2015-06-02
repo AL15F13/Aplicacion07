eb404af2cb5f13100923830a223956049c6262b9
Said <said_galindo@hotmail.com>
2015-06-02 13:46:35 -0500

    Cuatro
    Audio agregado

--- js/acciones.js
+++ js/acciones.js
@@ -1,6 +1,16 @@
 // JavaScript Document
 $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
+	audio = windows.plugins.LowLatencyAudio;
+	audio.preloadFX ('B1', 'audio/C.mp3', function () {},
+	    function(msg){alert("Error "+msg);});
+		audio.preloadFX ('B2', 'audio/D.mp3', function () {},
+	    function(msg){alert("Error "+msg);});
+		audio.preloadFX ('B3', 'audio/E.mp3', function () {},
+	    function(msg){alert("Error "+msg);});
+		audio.preloadFX ('B4', 'audio/F.mp3', function () {},
+	    function(msg){alert("Error "+msg);});
+		
 	$('#btn_jugar').on('tap', function(){
 		var pantalla = $.mobile.getScreenHeight();
 		var encabezado = $('.ui-header').outerHeight();
@@ -16,11 +26,12 @@ document.addEventListener("deviceready",function(){
 		
 		function quien (q)
 	{
-		return q.substring(1);
+		audio.play(q)
+		return q.substring(1); //Regresa la cadena (ID)a partir del caracter 01
 	}	
 		
 		$('.cuadro').on('tap', function (){
-			$('#pantalla').append(quien($(this).attr('id')));
+			$('#pantalla').append(quien($(this).attr('id'))); //Especifica la cadena (ID)
 			$(this).removeClass('pulsado');
 		});
 		
