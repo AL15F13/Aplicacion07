9c507eef0d9b672ee26639cea42d7a0e6499dbc5
Said <said_galindo@hotmail.com>
2015-06-10 13:41:12 -0500

    Ocho
    Falta corregir base de datos

--- js/acciones.js
+++ js/acciones.js
@@ -1,72 +1,88 @@
 // JavaScript Document
 $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
-	
-	CargarNombreJugador();
-	function CargarNombreJugador ()
+	var basedatos= window.sqlitePlugin.openDatabase ({name: "ColoresBD.db", createFromLocation:1});
+	cargarnombrejugador();
+	function cargarnombrejugador()
 	{
-		basedatos.transaction(function(ejecutar){
-		var sql = "SELECT NomberUsuario FROM Usuario";  //El campo se llama "NomberUsuario(no cambiar)"
-		ejecutar.executeSql(sql, undefined,
-		        function(ejecutar, resultado){
-		var datosJugador = resultado.rows.item(0);
-		$('#jugador').text(datosJugador.NombreUsuario);
-				});
-		});
+		basedatos.transaction (function (ejecutar){
+			var sql="SELECT NombreUsuario FROM Usuario";
+			ejecutar. executeSql (sql, undefined,function(ejecutar, resultado){
+				var datosJugador=resultado.rows.item(0);
+				$('#jugador').text(datosJugador.NombreUsuario);
+	
+	
+	});
+	cargarnombrejugador();
+	});
 	}
-	
-	var basedatos = window.sqlitePlugin.
-	openDatabase({name: "coloresBD.db",
-	createFromLocation: 1});
-	
-	audio = window.plugins.LowLatencyAudio;
-	audio.preloadFX ('B1', 'audio/C.mp3', function () {},
-	   function(msg){alert("Error "+msg);});
-		audio.preloadFX ('B2', 'audio/D.mp3', function () {},
-	    function(msg){alert("Error "+msg);});
-		audio.preloadFX ('B3', 'audio/E.mp3', function () {},
-	    function(msg){alert("Error "+msg);});
-		audio.preloadFX ('B4', 'audio/F.mp3', function () {},
-	    function(msg){alert("Error "+msg);});
-		
-	$('#btn_jugar').on('tap', function(){
-		var pantalla = $.mobile.getScreenHeight();
-		var encabezado = $('.ui-header').outerHeight();
-		var pie = $('.ui-footer').outerHeight();
-		var contenido = $('ui.content').outerHeight();
-		var alto = (pantalla - encabezado - pie)/2;
-		$('.cuadro').height(alto);
-		});//btnjugar.click
-		
-		$('.cuadro').on('tap', function(){
-			$(this).addClass('pulsado');
-			});//mousedown
-		
-		function quien (q)
-	{
-		audio.play(q)
-		return q.substring(1); //Regresa la cadena (ID)a partir del caracter 01
-	}	
-		
-		$('.cuadro').on('tap', function (){
-			$('#pantalla').append(quien($(this).attr('id'))); //Especifica la cadena (ID)
-			$(this).removeClass('pulsado');
+	$('#btnajustes').on('tap',function(){
+	$('#txtnombre').val($('#jugador').text());
+	});
+	$('#btnguardar').on('tap',function(){
+		var nuevonombre=$('#txtnombre').val();
+		basedatos.transaction(function(consulta){
+		consulta.executeSql("UPDATE Usuario SET NombreUsuario=? WHERE ClaveUsuario='1';",[nuevonombre]);
 		});
-		
-		$('#btn_configurar').on('tap', function(){
-			$('#txt_nombre').val($('#jugador').text());
-		});//BTN_Configurar
-		
-		$('#btn_guardar').on('tap', function(){
-			var nuevonombre = $('#txt_nombre').val();
-			basedatos.transaction (function(consulta){
-			  consulta.executeSql("UPDATE Usuario SET NombreUsuario = ? WHERE ClaveUsuario = '1';",[nuevonombre]);
-			
-			});
-			cargarnombrejugador();
-		});
+	});
+	
+function flash (boton){
+	boton.stop().animate({opacity:'0.5'},{
+		duration:80,
+		complete:function(){
+			boton.stop().animate({opacity:'1'},
+			200);
+		}
+	});
+}
+	audio=window.plugins.LowLatencyAudio;
+	audio.preloadFX('B1', 'audio/C.mp3', function(){}, function(msg){alert ("Error " + msg);});
+	audio.preloadFX('B2', 'audio/D.mp3', function(){}, function(msg){alert ("Error " + msg);});
+	audio.preloadFX('B3', 'audio/E.mp3', function(){}, function(msg){alert ("Error " + msg);});
+	audio.preloadFX('B4', 'audio/F.mp3', function(){}, function(msg){alert ("Error " + msg);});
+$('#btnjugar').on ('tap',function(){
+
+	var pantalla=$.mobile.getScreenHeight();
+	var encabezado=$('.ui-header').outerHeight();
+	var pie=$('.ui-footer').outerHeight();
+	var contenido=$('.ui-content').outerHeight();
+	//alert ('pantalla ' + pantalla);
+	//alert ('encabezado ' + encabezado);
+	//alert ('pie' + pie);
+	//alert ('contenido ' + contenido);
+	var alto=(pantalla-encabezado-pie)/2;
+	$('.cuadro').height(alto);
 	
 	
-});//device ready 
+	
+});//btnjugar
+
+
+//$('.cuadro').on ('vmousedown', function(){
+	//$('#pantalla').append (quien ($(this).attr('id')));
+	//$(this).addClass('pulsado');
+		
+    //});	
+	
+	
+	//$('.cuadro').on ('vmouseup', function(){
+	//$(this).removeClass('pulsado');	
+    //});	
+	
+	
+	function quien (q)
+ {
+	 audio.play(q);
+	 return q.substring(1);
+ }
+ 
+ $('.cuadro').on('tap', function(){
+	 flash($(this));
+	 audio.play($(this).attr('id'));
+ });
+
+});//cuadro
+
+ 
 });
 
